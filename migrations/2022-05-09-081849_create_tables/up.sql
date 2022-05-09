68b245f49766b25f042a1444c5a08f44b162b2eb
shugo256 <shugo256@gmail.com>
2022-05-09 18:29:40 +0900

    Setup Diesel.

---
+++ migrations/2022-05-09-081849_create_tables/up.sql
@@ -0,0 +1,28 @@
+-- Your SQL goes here
+CREATE TABLE movies (
+    `id` VARCHAR(10) PRIMARY KEY NOT NULL,
+    `title` VARCHAR(10) NOT NULL,
+    `genres` VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE ratings (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `user_id` VARCHAR(10) NOT NULL,
+    `movie_id` VARCHAR(10) NOT NULL,
+    `rating` DOUBLE NOT NULL,
+    `timestamp` INTEGER NOT NULL
+);
+
+CREATE TABLE tags (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `user_id` VARCHAR(10) NOT NULL,
+    `movie_id` VARCHAR(10) NOT NULL,
+    `name` VARCHAR(10) NOT NULL,
+    `timestamp` INTEGER NOT NULL
+);
+
+CREATE TABLE links (
+    `movie_id` VARCHAR(10) PRIMARY KEY NOT NULL,
+    `imdb_id` VARCHAR(10) NOT NULL,
+    `tmdb_id` VARCHAR(10) NOT NULL
+);
---
+++ src/schema.rs
@@ -0,0 +1,37 @@
+table! {
+    links (movie_id) {
+        movie_id -> Text,
+        imdb_id -> Text,
+        tmdb_id -> Text,
+    }
+}
+
+table! {
+    movies (id) {
+        id -> Text,
+        title -> Text,
+        genres -> Text,
+    }
+}
+
+table! {
+    ratings (id) {
+        id -> Nullable<Integer>,
+        user_id -> Text,
+        movie_id -> Text,
+        rating -> Double,
+        timestamp -> Integer,
+    }
+}
+
+table! {
+    tags (id) {
+        id -> Nullable<Integer>,
+        user_id -> Text,
+        movie_id -> Text,
+        name -> Text,
+        timestamp -> Integer,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(links, movies, ratings, tags,);
